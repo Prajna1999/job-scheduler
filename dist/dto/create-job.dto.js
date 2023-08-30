0e92e6ef8eaddb2637c6637e30a35587ce694b71
Prajna1999 <gituprajna20@gmail.com>
2023-08-30 23:28:41 +0530

    add scaffolding

---
+++ dist/dto/create-job.dto.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=create-job.dto.js.map
---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { JobsService } from './jobs/jobs.service';
+import { JobsController } from './jobs/jobs.controller';
+
+@Module({
+  imports: [],
+  controllers: [AppController, JobsController],
+  providers: [AppService, JobsService],
+})
+export class AppModule {}
